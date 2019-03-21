e7c1dfe97d71901a0026638605ad894cd128fa26
Cody-Price <ctprice11@gmail.com>
2019-03-21 15:10:23 -0600

    Initialize Workout Component

--- src/Components/Splash/Splash.js
+++ src/Components/Splash/Splash.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Route, NavLink } from 'react-router-dom'
+import { Route, NavLink, Switch } from 'react-router-dom'
+// import Workout from ''
 
 const Splash = () => {
 
@@ -8,6 +9,11 @@ const Splash = () => {
       <h1 className="splash-hdr">Pocket Spotter</h1>
       <NavLink to='/Workout' className="splash-btn">Workout Now</NavLink>
       <NavLink to='/Calendar' className="splash-btn">Calendar</NavLink>
+      <Switch>
+        {/* <Route exact path='/' component={Splash}/>
+        <Route exact path='/' component={Workout}/>
+        <Route exact path='/' component={Calendar}/> */}
+      </Switch>
     </div>
   )
 }
