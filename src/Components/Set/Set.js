275b3d1de12c6695f84b8fd73d3f4a1c28624632
Cody-Price <ctprice11@gmail.com>
2019-04-01 14:00:56 -0600

    Pivot to react-contenteditable

--- src/Components/Set/Set.js
+++ src/Components/Set/Set.js
@@ -4,10 +4,10 @@ const Set = ({set}) => {
 
   return (
     <div className="set">
-      {set.muscleGroup} 
-      {set.exerciseName} 
-      {set.leftOption} 
-      {set.rightOption} 
+      {/* <p className="set-group subset">{set.muscleGroup}</p>
+      <p className="set-name subset">{set.exerciseName}</p> */}
+      <p className="set-left subset">{set.leftOption}</p>
+      <p className="set-right subset">{set.rightOption}</p>
     </div>
   )
 }
